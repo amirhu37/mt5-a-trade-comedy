e67ee551ea8a1c1cf6535277e21c67bf8efb507b
Nemo <altair.infamous@yahoo.com>
2022-08-09 01:21:31 +0430

    trend line trade Scenario

--- Main/run.py
+++ Main/run.py
@@ -1,74 +1,94 @@
 import os
+import re
 import time
 import main
 from MetaTrader5 import initialize , positions_total
+from multiprocessing import Process
 
 initialize()
 
-SYMBOL = "ETHUSD"
+SYMBOL = "BTCUSD"
 VOLUME =  0.01
 TIMEFRAME =  1
-PERIOD = 150      
+PERIOD = 100   
+ma = 30    
 RR_RATIO = 2 
 DEVIATION = 20
-MINUTE = 60
-minutes = TIMEFRAME * MINUTE
 
-
-
-spread, point, _ = main.Symbol_info(SYMBOL)
 order = main.market_order
 
+spread, point, _ = main.Symbol_info(SYMBOL)
 
-Tr = main.Trend_np(SYMBOL, TIMEFRAME, 10)
+# def flag():
+#     print('flg')
+#     new = 0
+#     time.sleep(60*TIMEFRAME*(ma//2))
+#     new = 1
+#     return new
 
-slope, intr = Tr.trend_np()
 
-sup, res = main.SUPPORT_RESISTANCE(SYMBOL, TIMEFRAME, PERIOD, 3, 2).result()
 
-line = Tr.trend_line()
+def make():
+    print('T line')
+    Tr = main.Trend_reg(SYMBOL, TIMEFRAME, ma)
+    slopee ,_ = Tr.trend_reg()
+    line_1e, line_2e = Tr.trend_line(ma)
 
-# data = main.Symbol_data('BTCUSD',TIMEFRAME,PERIOD, 'o')
+    _, c2e= main.Symbol_data(SYMBOL, TIMEFRAME, 2, 'c')
+    # time.sleep(10)
+    return slopee, c2e, line_1e, line_2e
 
-o1 , o2 = main.Symbol_data(SYMBOL, TIMEFRAME, 2, 'o', True)
-l1 , l2 = main.Symbol_data(SYMBOL, TIMEFRAME, 2, 'l', True)
 
-h1 , h2 = main.Symbol_data(SYMBOL, TIMEFRAME, 2, 'h', True)
-c1 , c2 = main.Symbol_data(SYMBOL, TIMEFRAME, 2, 'c', True)
+
+
+def Main():
+    print('main') 
+    slope, c2, line_1, line_2 = make()   
+
+    print(slope,'\n' ,c2 )
+     
+    for i,j in list(zip(line_1, line_2)) :
+        # Line 1 => Main  line
+        # Line 2 => Proxy line
+        if slope > 0: # Positive Slope
+            if(c2 > j ) : # bUY
+                if not positions_total():
+                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'buy', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
+            
+            elif (c2 < i): # SELL
+                if not positions_total():
+                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'sell', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
+
+        elif slope < 0: # Negetive Slope
+            if ( c2 > i): # BUY
+                if not positions_total():
+                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'buy', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
+
+            elif (c2 < j ) : #SELL
+                if not positions_total():
+                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'sell', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
+            
+        
+        else:
+            pass
+        
 
 if __name__ == '__main__':
-    counter = 0
+
+    C = 0
+ 
     while True:
-        print(f"counter : {counter}")
-        print(f"spread : {spread} ")
-        print(f" slope : {slope}")
-        # print( c1, c2 )
-        print(f"t line : {line}")
-        print(f"sup: {sup} \nres: {res}")
+        print(C)
+        Main()
+        # pr1 = Process(target= Main)
+        # pr1.start()
 
-        for i, j in list(zip(line, sup)) :
-            # print(f"{i}: {sup}, {j}: {line}")
-            if o1 > i and slope < 0 and l1 <= j <= c1 :
+        # pr2 = Process(target= make)
+        # pr2.start()
 
-                if not positions_total():
-                
-                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'buy', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
-            else:
-                pass
-            
-        for i, j in list(zip(line, res)):
-            # print(f'{i}: {res}, {j}: {line}')
-            if c2 < i and slope > 0 and h1 >= j >= o1:
+        # pr1.join()
+        # pr2.join()
 
-                if not positions_total():
-
-                    order(symbol= SYMBOL, volume= VOLUME, order_type= 'sell', deviation= DEVIATION, SPREAD= spread, POINT= point, RATIO=RR_RATIO )
-            else:
-                pass
-            
-
-
-        
-        time.sleep(10)
+        time.sleep(1)
         os.system('cls')
-        counter += 1
+        C += 1
